47ddef0afca82daa65a37f2f84597319f958499d
Junior <nibi.amdukia@gmail.com>
2024-01-27 15:28:50 +0100

    ENH - Agregado de Svelte a la config de Astro && Gestion del Tema de la Aplicacion && Elemento Selected en el Header.astro

---
+++ src/svelte/MiComponente.svelte
@@ -0,0 +1,7 @@
+<!-- MiComponente.svelte -->
+<script>
+    let mensaje = '¡Hola desde Svelte!';
+  </script>
+  
+  <h1>{mensaje}</h1>
+  
---
+++ src/svelte/Navigation.svelte
@@ -0,0 +1,49 @@
+<script>
+  export let selectedLink = '';
+  
+  function handleLinkClick(link) {
+    selectedLink = link;
+    // Emitir un evento personalizado para informar al padre (header.astro) sobre el cambio de enlace seleccionado
+    dispatch('linkSelected', { selectedLink });
+  }
+</script>
+
+<style>
+  a {
+    /* Estilos normales de los enlaces */
+    color: inherit;
+    text-decoration: none;
+    padding: 0.5rem;
+    margin: 0.2rem;
+  }
+
+  .selected {
+    /* Estilos para el enlace seleccionado */
+    color: #ffffff;
+    background-color: #333333;
+    border-radius: 0.25rem;
+  }
+</style>
+
+<a
+  class="{selectedLink === '/#experiencia' ? 'selected' : ''}"
+  href="/#experiencia"
+  on:click={() => handleLinkClick('/#experiencia')}
+>Experiencia</a>
+<a 
+  class="{selectedLink === '/#proyectos' ? 'selected' : ''}"
+  href="/#proyectos"
+  on:click={() => handleLinkClick('/#proyectos')}
+>Proyectos</a>
+<a 
+  class="{selectedLink === '/#' ? 'selected' : ''}"
+  href="/#"
+  on:click={() => handleLinkClick('/#')}
+>Sobre mí</a>
+<a 
+  class="hover:bg-white/10 rounded-full px-3 transition {selectedLink === '/#contacto' ? 'selected' : ''}"
+  href="/#contacto"
+  on:click={() => handleLinkClick('/#contacto')}
+>Contacto</a>
+
+
