bb834408337949b466fd59ab638583f98b979eb5
Junior <nibi.amdukia@gmail.com>
2024-01-31 00:50:27 +0100

    NEW - Agregando i18n para la traduccion del portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), astroI18next()]
 });
