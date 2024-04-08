7704e18034af2105cd627b235fff3647a93402ed
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-04-09 01:47:22 +0200

    ENH - Agregado configuracion de robots astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import astroI18next from "astro-i18next";
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), astroI18next()]
+  integrations: [tailwind(), svelte(), astroI18next(), robotsTxt()],
+  site: 'https://cosmic-bublanina-bebbc0.netlify.app'
 });
