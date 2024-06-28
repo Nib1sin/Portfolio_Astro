654022f1af1f45cab52bdf5cb9585441b62e3996
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-06-28 09:18:15 +0200

    ENH - Mejorando la calidad de images, agregando traducciones y un nuevo proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import astroI18next from "astro-i18next";
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from "astro-robots-txt";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), astroI18next(), robotsTxt()],
+  integrations: [tailwind(), svelte(), astroI18next(), robotsTxt(), preact()],
   site: 'https://cosmic-bublanina-bebbc0.netlify.app'
 });
---
+++ src/react/ReadMore.tsx
@@ -0,0 +1,27 @@
+// Importaciones necesarias
+import { useState } from 'preact/hooks';
+
+// Componente ReadMore
+const ReadMore = ({ description }: { description: string }, maxLines = 4) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpand = () => setIsExpanded(!isExpanded);
+
+  // Estilo para limitar las líneas de texto visibles
+  const style = `display: -webkit-box; -webkit-line-clamp: ${maxLines}; -webkit-box-orient: vertical; overflow: hidden;`;
+
+  return (
+    <div>
+      <div class={isExpanded ? '' : 'line-clamp'} style={!isExpanded ? style : ''}>
+        {description}
+      </div>
+      <button class="text-blue-500 hover:underline" onClick={toggleExpand}>
+        {isExpanded ? 'Mostrar menos' : 'Seguir leyendo'}
+      </button>
+    </div>
+
+    
+  );
+};
+
+export default ReadMore;
+
