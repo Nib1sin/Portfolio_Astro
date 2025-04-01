0996cba21a4468e29eee3a2b8269e56e68f06bfc
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2025-04-01 03:00:27 +0200

    NEW - Actualizando Astro_4 a Astro_5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
 import astroI18next from "astro-i18next";
 import robotsTxt from "astro-robots-txt";
 
@@ -8,6 +7,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), astroI18next(), robotsTxt(), preact()],
-  site: 'https://cosmic-bublanina-bebbc0.netlify.app'
+  integrations: [tailwind(), astroI18next(), robotsTxt(), preact()],
+  site: 'https://pedro-develop.netlify.app/'
 });
